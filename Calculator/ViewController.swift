5ca6d38a941ae4a14b4ec67aa58d29ab886e396e
jpcunha4 <jpcunha4@gmail.com>
2018-06-23 23:38:20 -0300

    Basic portrait calculator

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var numberOnScreen:Double = 0
+    var previousNum:Double = 0
+    var performingMath = false;
+    var operation = 0
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,50 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func Numbers(_ sender: UIButton) {
+        if performingMath{
+            resultLabel.text = String(sender.tag - 1)
+            performingMath = false
+        }
+        
+        resultLabel.text = "\(resultLabel.text! + String(sender.tag - 1))"
+        numberOnScreen = Double(resultLabel.text!)!
+    }
+    
+    @IBAction func Buttons(_ sender: UIButton) {
+        if resultLabel.text != "" && sender.tag != 11 && sender.tag != 16{
+            previousNum = Double(resultLabel.text!)!
+            operation = sender.tag
+            
+            if operation == 12{
+                resultLabel.text = "/"
+            }else if operation == 13{
+                resultLabel.text = "x"
+            }else if operation == 14{
+                resultLabel.text = "-"
+            }else{
+                resultLabel.text = "+"
+            }
+            
+            performingMath = true
+        }else if sender.tag == 16{
+            if operation == 12{
+                resultLabel.text = "\(previousNum / numberOnScreen)"
+            }else if operation == 13{
+                resultLabel.text = "\(previousNum * numberOnScreen)"
+            }else if operation == 14{
+                resultLabel.text = "\(previousNum - numberOnScreen)"
+            }else{
+                resultLabel.text = "\(previousNum + numberOnScreen)"
+            }
+        }else if sender.tag == 11{
+            resultLabel.text = ""
+            performingMath = false
+            previousNum = 0
+            numberOnScreen = 0
+            operation = 0
+        }
+    }
+    
 }
 
